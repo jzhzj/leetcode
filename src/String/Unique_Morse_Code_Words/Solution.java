f30d82daac0b6fb682d50b94db6a208fa6ebd574
qijiuzhi <35633477+jzhzj@users.noreply.github.com>
2018-06-11 15:14:35 +0800

    Unique Morse Code Words

---
+++ src/String/Unique_Morse_Code_Words/Solution.java
@@ -0,0 +1,52 @@
+package String.Unique_Morse_Code_Words;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+ * <p>
+ * For convenience, the full table for the 26 letters of the English alphabet is given below:
+ * <p>
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+ * <p>
+ * Return the number of different transformations among all words we have.
+ * <p>
+ * Example:
+ * Input: words = ["gin", "zen", "gig", "msg"]
+ * Output: 2
+ * Explanation:
+ * The transformation of each word is:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ * <p>
+ * There are 2 different transformations, "--...-." and "--...--.".
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * The length of words will be at most 100.
+ * Each words[i] will have length in range [1, 12].
+ * words[i] will only consist of lowercase letters.
+ */
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] MORSE = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                "...-", ".--", "-..-", "-.--", "--.."};
+
+        Set<String> seen = new HashSet<>();
+        for (String word : words) {
+            StringBuilder code = new StringBuilder();
+            for (char c : word.toCharArray())
+                code.append(MORSE[c - 'a']);
+            seen.add(code.toString());
+        }
+
+        return seen.size();
+    }
+}
