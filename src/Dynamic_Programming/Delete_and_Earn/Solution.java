067854f2493778d7bbe278ab8014005564290d1e
qijiuzhi <35633477+jzhzj@users.noreply.github.com>
2018-06-13 13:41:18 +0800

    Delete and Earn

--- src/Dynamic_Programming/Delete_and_Earn/Solution.java
+++ src/Dynamic_Programming/Delete_and_Earn/Solution.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 /**
  * Given an array nums of integers, you can perform operations on the array.
- *
+ * <p>
  * In each operation, you pick any nums[i] and delete it to earn nums[i] points. After, you must delete every element equal to nums[i] - 1 or nums[i] + 1.
- *
+ * <p>
  * You start with 0 points. Return the maximum number of points you can earn by applying such operations.
- *
+ * <p>
  * Example 1:
  * Input: nums = [3, 4, 2]
  * Output: 6
@@ -23,15 +23,15 @@ import java.util.Arrays;
  * Then, delete 3 again to earn 3 points, and 3 again to earn 3 points.
  * 9 total points are earned.
  * Note:
- *
+ * <p>
  * The length of nums is at most 20000.
  * Each element nums[i] is an integer in the range [1, 10000].
- * */
+ */
 public class Solution {
     public int deleteAndEarn(int[] nums) {
         int[] count = new int[10001];
         Arrays.sort(nums);
-        for (int num: nums) {
+        for (int num : nums) {
             count[num]++;
         }
 
@@ -45,7 +45,7 @@ public class Solution {
                 if (i - 1 != prev) {
                     using = i * count[i] + m;
                     avoid = m;
-                }else {
+                } else {
                     using = i * count[i] + avoid;
                     avoid = m;
                 }
