a98dabd82e1cdce8beaeebc8ddb6bc46ba76eeb4
qijiuzhi <35633477+jzhzj@users.noreply.github.com>
2018-06-17 15:00:10 +0800

    Maximum Depth of Binary Tree

--- src/Tree/Average_of_Levels_in_Binary_Tree/Solution.java
+++ src/Tree/Average_of_Levels_in_Binary_Tree/Solution.java
@@ -1,32 +1,9 @@
 package Tree.Average_of_Levels_in_Binary_Tree;
 
+import Tree.TreeNode;
+
 import java.util.LinkedList;
 import java.util.List;
-
-/**
- * Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
- * Example 1:
- * Input:
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- * Output: [3, 14.5, 11]
- * Explanation:
- * The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
- * Note:
- * The range of node's value is in the range of 32-bit signed integer.
- * */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
 
 public class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
