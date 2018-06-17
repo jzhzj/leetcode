7820402a231045aed8db9876e09e8fd670f6cdbf
qijiuzhi <35633477+jzhzj@users.noreply.github.com>
2018-06-17 13:56:21 +0800

    Average of Levels in Binary Tree

---
+++ src/Tree/Average_of_Levels_in_Binary_Tree/Solution.java
@@ -0,0 +1,51 @@
+package Tree.Average_of_Levels_in_Binary_Tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+/**
+ *
+ * */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<TreeNode> level = new LinkedList<>();
+        level.add(root);
+        List<Double> result = new LinkedList<>();
+        result.add((double) root.val);
+        while (true) {
+            List<TreeNode> prevLevel = new LinkedList<>(level);
+            level.clear();
+            int size = prevLevel.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode remove = ((LinkedList<TreeNode>) prevLevel).remove();
+                if (remove.left != null)
+                    level.add(remove.left);
+                if (remove.right != null)
+                    level.add(remove.right);
+            }
+            if (level.isEmpty())
+                break;
+            result.add(avg(level));
+        }
+        return result;
+    }
+
+    private double avg(List<TreeNode> level) {
+        int size = level.size();
+        long sum = 0;
+        for (TreeNode node : level) {
+            sum += node.val;
+        }
+        return (double) sum / size;
+    }
+}
