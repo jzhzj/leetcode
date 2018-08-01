6d08526b7e0403c6f6f5fa5056fb4b6d3b592910
qijiuzhi <35633477+jzhzj@users.noreply.github.com>
2018-08-01 15:55:21 +0800

    N-ary Tree Level Order Traversal

--- src/Breadth_First_Search/N_ary_Tree_Level_Order_Traversal/Solution.java
+++ src/Breadth_First_Search/N_ary_Tree_Level_Order_Traversal/Solution.java
@@ -6,7 +6,31 @@ import java.util.List;
 import java.util.Queue;
 
 /**
+ * Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
  *
+ * For example, given a 3-ary tree:
+ *
+ *
+ *
+ *
+ *
+ * We should return its level order traversal:
+ *
+ *
+ *
+ *
+ *
+ * [
+ *      [1],
+ *      [3,2,4],
+ *      [5,6]
+ * ]
+ *
+ *
+ * Note:
+ *
+ * The depth of the tree is at most 1000.
+ * The total number of nodes is at most 5000.
  * */
 class Node {
     public int val;
